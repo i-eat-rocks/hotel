4d763a7b5aff4813d64b3e8f70c89b92b6dd80a2
i-eat-rocks <143463845+i-eat-rocks@users.noreply.github.com>
2023-11-01 00:30:44 -0500

    Add address form

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,37 @@
 $(function () {
+    // using states json from https://gist.github.com/mshafrir/2646763
+    $.getJSON('./lib/states.json', function(data) {
+        $.each(data, function(abbr, stateName) {
+            $('#state').append($('<option>', {
+                value: abbr,
+                text: stateName
+            }));
+        });
+    });
+    // TODO: add address info to form, save to cookie (check how vdub did it)
+    // TODO: validate addy info if check is checked
     $('#newsletterForm').submit(function (e) {
         e.preventDefault();
-
-        var formData = {
-            firstName: $('#firstName').val(),
-            lastName: $('#lastName').val(),
-            birthdate: $('#birthdate').val(),
-            gender: $('input[name=gender]:checked').val(),
-            honorific: $('#honorific').val(),
-            email: $('#email').val(),
-            additionalInfo: $('#additionalInfo').val()
-        };
-    });
+        //     var formData = {
+            //         firstName: $('#firstName').val(),
+            //         lastName: $('#lastName').val(),
+            //         birthdate: $('#birthdate').val(),
+            //         gender: $('#gender').val(),
+            //         honorific: $('#honorific').val(),
+            //         email: $('#email').val(),
+            //         additionalInfo: $('#additionalInfo').val()
+            //     };
+        });
+        $('#receivePaperNewsletter').change(function() {
+            if (this.checked) {
+                $('#addressFields').show();
+                toggleAddressFieldsRequired();
+            } else {
+                $('#addressFields').hide();
+                toggleAddressFieldsRequired();
+            }
+        });
+    function toggleAddressFieldsRequired() {
+        
+    }
 });
